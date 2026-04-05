a379e8ab77e55f1e19c33cd6bd7d3460aaad8df8
Tibo De Peuter <tibo.depeuter@ugent.be>
2026-04-05 16:30:04 +0200

    feat(website): setup

---
+++ website/observablehq.config.js
@@ -0,0 +1,21 @@
+export default {
+  title: "NATO Strategic Capacities Visualised",
+
+  root: "src",
+
+  // logo: "logo.png",
+
+  // favicons: [
+  //   {rel: "icon", href: "favicon.ico", sizes: "any"},
+  //   {rel: "icon", href: "favicon.svg", type: "image/svg+xml"}
+  // ],
+
+  sidebar: true,
+  toc: true,
+  pager: true,
+
+  // search: true,
+
+  // TODO Add names
+  footer: `Built with ❤️`,
+};
